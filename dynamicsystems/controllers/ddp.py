c083ef0f791488e03b49169174ef374e13a63d92
Clark Zhang <clarkz@seas.upenn.edu>
2017-12-08 17:05:58 -0500

    completed basic ddp implementation

--- dynamicsystems/controllers/ddp.py
+++ dynamicsystems/controllers/ddp.py
@@ -3,7 +3,7 @@ import scipy.linalg
 import math
 import sys
 sys.path.append('../')
-from ode import ode_solver_once
+from ode import ode_solver_once, ode_solver_once_adaptive
 
 
 def numerical_quadraticization(x0, u0, func):
@@ -123,35 +123,40 @@ def numerical_quadraticization2(x0, func):
 
 
 
-
-
 # differential dynamic programming
 class DDP(object):
     def __init__(self, sys):
         self.sys = sys
         
-    def solve(self, x0, N, dt, quad_step_cost, quad_final_cost):
+    def solve(self, x0, N, dt, quad_step_cost_func, quad_final_cost_func):
         n = len(x0)
         m = 1
 
         controller_list = [(np.zeros((m, n)), np.zeros((m, 1))) for _ in range(N)]
         x_list = [np.zeros((n, 1)) for _ in range(N)]
+        x_list[0] = x0
         u_list = [np.zeros((m, 1)) for _ in range(N)]
 
-        for i in range(5):
-            x_list, u_list = self._forward_pass(x0, x_list, u_list, controller_list, dt)
-            controller_list = self._backward_pass(x_list, u_list, dt, quad_step_cost, quad_final_cost)
 
-            # print "x_list:"
-            # print(np.array(x_list).squeeze())
 
-            # print "u_list:"
-            # print(np.array(u_list).squeeze())
+        for i in range(50):
+            x_list, u_list = self._forward_pass(x_list, u_list, controller_list, dt)
+            controller_list = self._backward_pass(x_list, u_list, dt, quad_step_cost_func, quad_final_cost_func)
+            # x_arr = np.array(x_list)
+            # t = np.ones(N+1) * dt
+            # t = np.cumsum(t)
+            # plt.cla()
+            # plt.plot(t, x_arr[:, 0], label='theta', c='b')
+            # plt.plot(t, x_arr[:, 1], label='theta_dot', c='r')
+            # plt.legend()
+            # plt.show(block=False)
+            # plt.pause(0.01)
+            # raw_input('Press Enter to Continue: ')
 
         return controller_list, x_list, u_list
 
-    def _forward_pass(self, x0, x_bar_list, u_bar_list, controller_list, dt):
-        x_list = [x0]
+    def _forward_pass(self, x_bar_list, u_bar_list, controller_list, dt):
+        x_list = [x_bar_list[0]]
         u_list = []
         for i, (x_bar, u_bar, controller) in enumerate(zip(x_bar_list, u_bar_list, controller_list)):
             x = x_list[-1]
@@ -164,19 +169,19 @@ class DDP(object):
         return x_list, u_list
 
 
-    def _backward_pass(self, x_list, u_list, dt, quad_step_cost, quad_final_cost):
+    def _backward_pass(self, x_list, u_list, dt, quad_step_cost_func, quad_final_cost_func):
         controller_list = []
 
-        f0, b, A = quad_final_cost(x_list[-1])
+        f0, b, A = quad_final_cost_func(x_list[-1])
         for x, u in reversed(zip(x_list, u_list)):
             f0, A_sys, b_sys = self.sys.get_linearization(x, u)
 
             # discretize our linearized system
-            f_x = np.eye(A.shape[0]) + A_sys * dt
+            f_x = np.eye(A_sys.shape[0]) + A_sys * dt
             f_u = b_sys * dt
 
             # quadraticize costs
-            f0, lx, lu, lxx, luu, lux = quad_step_cost(x, u)
+            f0, lx, lu, lxx, luu, lux = quad_step_cost_func(x, u)
 
             qx = lx + np.dot(f_x.T, b)
             qu = lu + np.dot(f_u.T, b)
@@ -201,25 +206,57 @@ if __name__ == '__main__':
     from functools import partial
     import matplotlib.pyplot as plt
 
-    def final_cost(x, target):
-        cost = np.linalg.norm(x - target)
-        # cost = np.sum(np.square(x - target))
+
+    Cx = np.array([[100., 0], [0, 1]])
+    # Cu = np.array([[490.]])
+    Cu = np.array([[0.01]])
+
+    def final_cost(x, target_state, Cx=Cx):
+        delta_x = x - target_state
+        cost = np.dot(delta_x.T, np.dot(Cx, delta_x))
         return cost
 
+    def quad_final_cost(x, target_state, Cx=Cx):
+        A = 2*Cx
+        b = 2*np.dot(Cx, x - target_state)
+        f0 = final_cost(x, target_state)
+        return f0, b, A
 
-    def step_cost(x, u, target):
-        cost = np.linalg.norm(x - target)**2
-        cost += 2*np.linalg.norm(u)**2
-        return cost    
+    def step_cost(x, u, target_state, target_control, Cx=Cx, Cu=Cu):
+        delta_x = x - target_state
+        delta_u = u - target_control
+
+        cost = np.dot(delta_x.T, np.dot(Cx, delta_x))
+
+        cost += np.dot(delta_u.T, np.dot(Cu, delta_u))
+        return cost 
+
+    def quad_step_cost(x, u, target_state, target_control, Cx=Cx, Cu=Cu):
+        delta_x = x - target_state
+        delta_u = u - target_control
+
+        lx = 2*np.dot(Cx, delta_x)
+        lxx = 2*Cx
+        lu = 2*np.dot(Cu, delta_u)
+        luu = 2*Cu
+
+        m = len(u)
+        n = len(x)
+        lux = np.zeros((m, n))
+        f0 = step_cost(x, u, target_state, target_control)
+        return f0, lx, lu, lxx, luu, lux
 
 
-    target = np.array([[0, 0.]]).T
-    step_cost_func = partial(step_cost, target=target)
-    quad_step_cost = partial(numerical_quadraticization, func=step_cost_func)
 
 
-    final_cost_func = partial(final_cost, target=target)
-    quad_final_cost = partial(numerical_quadraticization2, func=final_cost_func)
+    target_state = np.array([[0, 0.]]).T
+    target_control = np.array([[0.]])
+    step_cost_func = partial(step_cost, target_state=target_state, target_control=target_control)
+    quad_step_cost_func = partial(quad_step_cost, target_state=target_state, target_control=target_control)
+
+    final_cost_func = partial(final_cost, target_state=target_state)
+    quad_final_cost_func = partial(quad_final_cost, target_state=target_state)
+
 
 
     dt = 0.05
@@ -228,12 +265,47 @@ if __name__ == '__main__':
     pendulum.set_state(x0)
     ddp = DDP(pendulum)
 
-    ddp.solve(x0, 100, dt, quad_step_cost, quad_final_cost)
 
+    # # open loop controller
+    # N = 500
+    # controller_list, x_list, u_list = ddp.solve(x0, N, dt, quad_step_cost_func, quad_final_cost_func)
+    
+    # states = np.zeros((2, N-1))
+    # controls = np.zeros(N-1)
+    # for i in range(N-1):
+    #     state = pendulum.get_state()
+    #     states[:, i] = state.squeeze()
+
+    #     controller = controller_list[0]
+    #     u = np.dot(controller[0], state - x_list[0]) + controller[1] + u_list[0]
+    #     # u = u_list[0]
+
+    #     pendulum.step(u)
+    #     controls[i] = u.squeeze()
+
+    # t = np.ones(N-1) * dt
+    # t = np.cumsum(t)
+    # plt.plot(t, states[0, :], label='theta')
+    # plt.plot(t, states[1, :], label='theta_dot')
+    # plt.plot(t, controls, label='control')
+    # plt.legend()
+    # plt.show()
+
+    # # plotting original ddp solution
+    # t = np.ones(N) * dt
+    # t = np.cumsum(t)
+    # x_arr = np.array(x_list).squeeze()
+    # plt.plot(t, x_arr[:-1, 0], label='theta')
+    # plt.plot(t, x_arr[:-1, 1], label='theta_dot')
+    # plt.plot(t, np.array(u_list).squeeze(), label='control')
+    # plt.legend()
+    # plt.show()
     # exit()
 
-    N = 100
-    ddp_horizon = 30
+
+    # closed loop control
+    N = 30
+    ddp_horizon = 20
     states = np.zeros((2, N-1))
     controls = np.zeros(N-1)
     for i in range(N-1):
@@ -241,7 +313,7 @@ if __name__ == '__main__':
         state = pendulum.get_state()
         states[:, i] = state.squeeze()
 
-        controller_list, x_list, u_list = ddp.solve(x0, ddp_horizon, dt, quad_step_cost, quad_final_cost)
+        controller_list, x_list, u_list = ddp.solve(x0, ddp_horizon, dt, quad_step_cost_func, quad_final_cost_func)
         controller = controller_list[0]
         u = np.dot(controller[0], state - x_list[0]) + controller[1] + u_list[0]
 
@@ -249,9 +321,9 @@ if __name__ == '__main__':
         controls[i] = u.squeeze()
 
 
-
     t = np.ones(N-1) * dt
     t = np.cumsum(t)
+
 
 
     plt.plot(t, states[0, :], label='theta')
